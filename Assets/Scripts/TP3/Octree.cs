a3a48b0c05715a6d765b146d053d42625db47973
Nicolas Graveret <neylick@gmail.com>
2025-11-24 15:23:16 +0100

    24-11 : TP3 continuation, just need to fix painting

--- Assets/Scripts/TP3/Octree.cs
+++ Assets/Scripts/TP3/Octree.cs
@@ -1,81 +1,171 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using Unity.Burst;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Octree : MonoBehaviour
 {
     [SerializeField] private int maxDepth = 4;
+    [SerializeField] private float CubeScale = 1;
     [SerializeField] private Vector3Int RootSize;
+    [SerializeField] private Material CubeMaterial;
     private Bounds RootBounds;
     private OctreeNode Root;
 
+    private SDFObject SceneTree;
+
+    private GameObject PaintTool;
+
+    InputAction addPaintAction, removePaintAction;
+
     // https://iquilezles.org/articles/distfunctions/
-    float dot2(Vector3 a) { return Vector3.Dot(a, a); }
-    
+    static float Dot2(Vector3 a) { return Vector3.Dot(a, a); }
+
     // Boolean operations
     // OR
-    float sdfUnion(float sdf1, float sdf2)
+    private static float UnionSDF(float sdf1, float sdf2)
     {
         return Mathf.Min(sdf1, sdf2);
     }
     // AND
-    float sdfIntersection(float sdf1, float sdf2)
+    private static float IntersectionSDF(float sdf1, float sdf2)
     {
         return Mathf.Max(sdf1, sdf2);
     }
     // XOR
-    float sdfXOR(float sdf1, float sdf2)
+    private static float XorSDF(float sdf1, float sdf2)
     {
-        return Mathf.Max(sdfUnion(sdf1, sdf2), -sdfIntersection(sdf1, sdf2));
+        return IntersectionSDF(UnionSDF(sdf1, sdf2), -IntersectionSDF(sdf1, sdf2));
     }
-    // Substract
-    float sdfSubstract(float sdf1, float sdf2)
+    // Substract ( AND NOT )
+    private static float SubstractSDF(float sdf1, float sdf2)
     {
-        return Mathf.Max(-sdf1, sdf2);
-    }
-
-    // Primitives
-
-    // Sphere with center and radius
-    private float SphereSDF(Vector3 p, Vector3 center, float radius)
-    {
-        return Mathf.Sqrt(dot2(p - center)) - radius;
-    }
-
-    //
-    private float BoxSDF(Vector3 p, Vector3 center, Vector3 halfsize)
-    {
-        Vector3 diff = p - center;  
-        Vector3 q = new Vector3(Mathf.Abs(diff.x), Mathf.Abs(diff.y), Mathf.Abs(diff.z)) - halfsize;
-        return Vector3.Magnitude(Vector3.Max(q, Vector3.zero)) + Mathf.Min(Mathf.Max(q.x, Mathf.Max(q.y, q.z)), 0.0f);
+        return IntersectionSDF(sdf1, -sdf2);
     }
 
     float SceneSDF(Vector3 p)
     {
-        float MAX = Mathf.NegativeInfinity;
-
-        // Unique sphere in center 
-        //MAX = Mathf.Max(MAX, SphereSDF(p, transform.position, 0.3f));
-
-        // Unique box in center
-        //MAX = Mathf.Max(MAX, BoxSDF(p, transform.position, new Vector3(.4f, .4f, .4f)));
-
-        // Intersection of 2 spheres
-        //MAX = Mathf.Max(MAX, sdfIntersection(
-        //    SphereSDF(p, transform.position + new Vector3(0.2f, 0.0f, 0.0f), 0.4f),
-        //    SphereSDF(p, transform.position + new Vector3(-0.2f, 0.0f, 0.0f), 0.4f)
-        //));
-
-        // Box - Sphere
-        MAX = Mathf.Max(MAX, sdfSubstract(
-            SphereSDF(p, transform.position + new Vector3(0.4f, 0, 0), 0.5f)
-            ,
-            BoxSDF(p, transform.position + new Vector3(0.0f, 0, 0), new Vector3(.4f, .4f, .4f))
-        ));
-
-        return MAX;
+        return SceneTree.SDF(p);
     }
-    
+
+    public enum OperationsOnSDF { Union, Intersection, Substraction, Xor };
+
+    public abstract class SDFObject
+    {
+        public abstract float SDF(Vector3 p);
+        public abstract Bounds GetBounds();
+        public abstract void DrawGizmos();
+    }
+
+    public class SphereSDF : SDFObject
+    {
+        private Vector3 center;
+        private float radius;
+
+        public SphereSDF(Vector3 center, float radius) 
+        {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        public override float SDF(Vector3 p)
+        {
+            return Mathf.Sqrt(Dot2(p - center)) - radius;
+        }
+
+        public override Bounds GetBounds()
+        {
+            return new Bounds(center, Vector3.one * radius * 2.0f);
+        }
+
+        public override void DrawGizmos()
+        {
+            Gizmos.DrawWireSphere(center, radius);
+        }
+    }
+
+    public class BoxSDF : SDFObject
+    {
+        private Vector3 center;
+        private Vector3 halfsize;
+        public BoxSDF(Vector3 center, Vector3 halfsize)
+        {
+            this.center = center;
+            this.halfsize = halfsize;
+        }
+
+        public override float SDF(Vector3 p)
+        {
+            Vector3 diff = p - center;
+            Vector3 q = new Vector3(Mathf.Abs(diff.x), Mathf.Abs(diff.y), Mathf.Abs(diff.z)) - halfsize;
+            return Vector3.Magnitude(Vector3.Max(q, Vector3.zero)) + Mathf.Min(Mathf.Max(q.x, Mathf.Max(q.y, q.z)), 0.0f);
+        }
+
+        public override Bounds GetBounds()
+        {
+            return new Bounds(center, halfsize * 2.0f);
+        }
+
+        public override void DrawGizmos()
+        {
+            Gizmos.DrawWireCube(center, halfsize * 2.0f);
+        }
+    }
+
+    public class CombinationSDF : SDFObject
+    {
+        private SDFObject a, b;
+        private OperationsOnSDF operation;
+        private Bounds? bounds = null;
+
+        public CombinationSDF(SDFObject a, SDFObject b, OperationsOnSDF operation)
+        {
+            this.a = a;
+            this.b = b;
+            this.operation = operation;
+            this.bounds = GetBounds();
+        }
+
+        public override float SDF(Vector3 p)
+        {
+            switch (operation)
+            {
+                case OperationsOnSDF.Union: return UnionSDF(a.SDF(p), b.SDF(p));
+                case OperationsOnSDF.Intersection: return IntersectionSDF(a.SDF(p), b.SDF(p));
+                case OperationsOnSDF.Substraction: return SubstractSDF(a.SDF(p), b.SDF(p));
+                case OperationsOnSDF.Xor: return XorSDF(a.SDF(p), b.SDF(p));
+                default: return Mathf.Infinity;
+            }
+        }
+        public override Bounds GetBounds()
+        {
+            if (bounds != null) return bounds.Value;
+            // On substraction, the substacted object shouldn't be used for bounds
+            else if (operation == OperationsOnSDF.Substraction) 
+            {
+                bounds = a.GetBounds();
+                return bounds.Value;
+            }
+            else
+            {
+                Bounds ba, bb;
+                ba = a.GetBounds();
+                bb = b.GetBounds();
+                Vector3 min = Vector3.Min(a.GetBounds().min, b.GetBounds().min);
+                Vector3 max = Vector3.Max(a.GetBounds().max, b.GetBounds().max);
+                bounds = new Bounds((min + max) / 2.0f, max - min);
+                return bounds.Value;
+            }
+        }
+
+        public override void DrawGizmos()
+        {
+            a.DrawGizmos();
+            b.DrawGizmos();
+        }
+    }
 
     class OctreeNode
     {
@@ -87,6 +177,7 @@ public class Octree : MonoBehaviour
         {
             return (children == null) || (depth <= 0);
         }
+        public bool IsFilled() { return value > 7.5; }
         public OctreeNode(Bounds bounds, int depth, int value = 0)
         {
             //Debug.Log("Creating node at depth " + depth + " with bounds center " + bounds.center + " and size " + bounds.size);
@@ -112,25 +203,22 @@ public class Octree : MonoBehaviour
         }
     }
 
-    bool IsFilled(float value) { return value > 7.9; }
-
     void FillOctree(OctreeNode node)
     {
         if(node.IsLeaf()) return;
         else
         {
+            node.value = 0;
             if (node.depth == 1)
             {
                 node.value = (SceneSDF(node.bounds.center) <= 0) ? 8 : 0;
-                Debug.Log(node.value);
             }
             else
             {
-                node.value = 0.0f;
                 for (int i = 0; i < node.children.Length; i++)
                 {
                     FillOctree(node.children[i]);
-                    node.value += (IsFilled(node.children[i].value) ? 1 : 0);
+                    node.value += node.children[i].IsFilled() ? 1 : 0;
                 }
             }
         }
@@ -138,33 +226,133 @@ public class Octree : MonoBehaviour
 
     void ModelOctree(OctreeNode node)
     {
-        Debug.Log($"Depth:{node.depth}, Value:{node.value}");
         if (node.IsLeaf()) return;
         else
-        {
-            bool shouldFill = IsFilled(node.value);
-            if (!shouldFill) // Check children
+        {;
+            if (!node.IsFilled()) // Check children
             {
-                for (int i = 0; i < node.children.Length; i++)
-                    ModelOctree(node.children[i]);
+                for (int i = 0; i < node.children.Length; i++) ModelOctree(node.children[i]);
             }
             else // Fill this node
             {
                 GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                Destroy(cube.GetComponent<BoxCollider>());
+                
                 cube.transform.position = node.bounds.center;
-                cube.transform.localScale = node.bounds.size;
+                cube.transform.localScale = node.bounds.size * CubeScale;
                 cube.transform.parent = this.transform;
+
+                //////////////// Debug color for cube depth ////////////////
+                //MeshRenderer renderer = cube.GetComponent<MeshRenderer>();
+                //renderer.material = new(CubeMaterial);
+                ////renderer.material.color = Color.HSVToRGB(360.0f * node.depth / (float) maxDepth, node.depth / (float) maxDepth, 1.0f);
+                //renderer.material.color = Color.green * node.depth / (float) maxDepth;
             }
         }
+    }
+
+    private void _PropagateValue(OctreeNode node, float value, Vector3 position)
+    {
+        if (node != null && node.bounds.Contains(position))
+        {
+            if (node.depth == 1) node.value += value;
+            else
+            {
+                node.value = 0;
+                foreach (OctreeNode n in node.children)
+                {
+                    _PropagateValue(n, value, position);
+                    node.value += n.value;
+                }
+            }
+        }
+    }
+
+    void UpdateOctree(OctreeNode node, float value, Vector3 position)
+    {
+        _PropagateValue(node, value, position);
+        foreach (Transform child in transform) Destroy(child.gameObject);
+        ModelOctree(node);
+    }
+
+    void InitOctree()
+    {
+        //RootBounds = new Bounds(transform.position, RootSize);
+        RootBounds = SceneTree.GetBounds();
+        Root = new OctreeNode(RootBounds, maxDepth);
+        FillOctree(Root);
+        ModelOctree(Root);
+    }
+
+    void InitScene()
+    {
+        // Unique sphere in center 
+        //SphereSDF sphereA = new SphereSDF(transform.position, 0.3f);
+        //SceneTree = sphereA;
+
+        // Unique box in center
+        //BoxSDF boxA = new BoxSDF(transform.position, new(.4f, .4f, .4f));
+        //SceneTree = boxA;
+
+        // Intersection of 2 spheres
+        //SphereSDF sphereB = new SphereSDF(transform.position + new Vector3( .2f, 0, 0), .4f);
+        //SphereSDF sphereC = new SphereSDF(transform.position + new Vector3(-.2f, 0, 0), .4f);
+        //SceneTree = new CombinationSDF(sphereB, sphereC, OperationsOnSDF.Intersection);
+
+
+        // Box - Sphere
+
+        SphereSDF sphereD = (new SphereSDF(transform.position + new Vector3(0, 0, -0.5f), .5f));
+        BoxSDF box = (new BoxSDF(transform.position, new Vector3(.5f, .5f, .5f)));
+        SceneTree = new CombinationSDF(box, sphereD, OperationsOnSDF.Substraction);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        RootBounds = new Bounds(transform.position, RootSize);
-        Root = new OctreeNode(RootBounds, maxDepth);
-        FillOctree(Root);
-        ModelOctree(Root);
+        addPaintAction = InputSystem.actions.FindAction("AddPaint");
+        removePaintAction = InputSystem.actions.FindAction("RemovePaint");
+
+        InitScene();
+        InitOctree();
+        PaintTool = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        PaintTool.transform.localScale = Vector3.one / 10.0f;
+        PaintTool.GetComponent<Collider>().enabled = false;
+        PaintTool.GetComponent<MeshRenderer>().material = CubeMaterial;
+    }
+
+    private void Update()
+    {
+        // shoot ray from camera to mouse going into the scene
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        //Ray ray = new Ray(Camera.main.transform.position, Vector3.Normalize(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - Camera.main.transform.position));
+        // cast ray
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 10))
+        {
+            PaintTool.transform.position = hit.point;
+        }
+
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+
+        if (addPaintAction.IsPressed())
+        {
+            // add matter
+            UpdateOctree(Root, 0.01f, PaintTool.transform.position);
+            Debug.Log("Drawing");
+        }
+        else if(removePaintAction.IsPressed())
+        {
+            // remove matter
+            UpdateOctree(Root, -0.01f, PaintTool.transform.position);
+            Debug.Log("Erasing");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(RootBounds.center, RootBounds.size);
+        Gizmos.color = Color.cyan;
+        if (SceneTree != null) SceneTree.DrawGizmos();
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Modélisation Géométrique
 (For now) Objects are constructed on startup, meaning the "game" needs to be restarted to change parameters.
 Positions can be altered via inspector in game and are updated.
-The scene can be easily navigated in the *Scene* window (and not *Game* window).
+Each scene can be easily navigated in the *Scene* window (and not *Game* window).
+Scenes for each work can be found in the scene directory
 ## TP1
 Scripts : (`*.cs`)
 - CustomQuad
@@ -20,3 +21,9 @@ Button exports the mesh as an off following the given path and file name.
 
 ![ImageTP2](Renders/TP2.png)
 
+## TP3
+Scripts : (`*.cs`)
+- Octree
+Loads an octree defined by bounds and depth (subdivision, 8 per depth).
+The rendered object inside are defined by the `SceneSDF` function (each object is represented by a signed distance function).
+Note : it would be best to use Unity Objects that later are converted via their parameters to SDFs.
